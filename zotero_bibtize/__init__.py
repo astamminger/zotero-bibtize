db3f0d5710d9a7051313150a82a20ee242205f5a
A. Stamminger <andreas.stamminger@pm.me>
2019-12-04 23:06:43 +0100

    add KeyFormatter to imported modules

--- zotero_bibtize/__init__.py
+++ zotero_bibtize/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from zotero_bibtize.zotero_bibtize import BibTexFile
+from zotero_bibtize.bibkey_formatter import KeyFormatter
 
-__all__ = ['BibTexFile']
+__all__ = ['BibTexFile', 'KeyFormatter']
