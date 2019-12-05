0be0b1cf1d3ff34fc8fa40684ec6d3efe3562055
A. Stamminger <andreas.stamminger@pm.me>
2019-12-05 13:00:14 +0100

    interface custom key generation with BibTexFile

--- zotero_bibtize/__init__.py
+++ zotero_bibtize/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from zotero_bibtize.zotero_bibtize import BibTexFile
-from zotero_bibtize.bibkey_formatter import KeyFormatter
 
-__all__ = ['BibTexFile', 'KeyFormatter']
+__all__ = ['BibTexFile']
--- zotero_bibtize/zotero_bibtize.py
+++ zotero_bibtize/zotero_bibtize.py
@@ -2,15 +2,18 @@
 
 
 import re
+import collections
+
+from zotero_bibtize.bibkey_formatter import KeyFormatter
 
 
 class BibEntry(object):
-    def __init__(self, bibtex_entry_string):
+    def __init__(self, bibtex_entry_string, key_format):
         self._raw = bibtex_entry_string
         entry_type, entry_key, entry_fields = self.entry_fields(self._raw)
         # set internal variables
         self.type = entry_type
-        self.key = entry_key
+        self.key = KeyFormatter(entry_fields).generate_key(key_format)
         self.fields = entry_fields
         
     def entry_fields(self, bibtex_entry_string):
@@ -102,11 +105,15 @@ class BibEntry(object):
 
 class BibTexFile(object):
     """Bibtext file contents"""
-    def __init__(self, bibtex_file):
+    def __init__(self, bibtex_file, key_format):
         self.bibtex_file = bibtex_file
         self.entries = []
-        for entry in self.parse_bibtex_entries():
-            self.entries.append(BibEntry(entry))
+        self.key_map = collections.defaultdict(list)
+        for (index, entry) in enumerate(self.parse_bibtex_entries()):
+            entry = BibEntry(entry, key_format)
+            self.entries.append(entry)
+            self.key_map[entry.key].append(index)
+        self.resolve_unambiguous_keys()
 
     def parse_bibtex_entries(self):
         """Parse entries from file."""
@@ -141,3 +148,26 @@ class BibTexFile(object):
                         stack += 1
                 bibtex_entries.append((start_index, next_index+1))
         return bibtex_entries
+
+    def num_to_char(self, number):
+        """
+        Map the given number on chars a-z.
+
+        All numbers N for 0 <= N <= 25 will be mapped on the chars a-z
+        and numbers N > 25 will be mapped on the chars aa-zz.
+
+        :param int number: number transformed to char representation
+        """
+        offset = ord('a')
+        minor = number % 26
+        major = number // 26 - 1
+        return chr(offset + major) * (major >= 0)  + chr(offset + minor)
+
+    def resolve_unambiguous_keys(self):
+        """Resolve ambiguous bibtex keys."""
+        for (key, indices) in self.key_map.items():
+            # do nothing if the key is unique already
+            if len(indices) == 1: continue
+            # otherwise append a-z / aa-zz to the key
+            for (i, index) in enumerate(indices):
+                self.entries[index].key = key + self.num_to_char(i)
