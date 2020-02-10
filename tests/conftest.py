5749281cd34e922bfa644f3e8573e7a6998ecb12
A. Stamminger <andreas.stamminger@pm.me>
2020-02-10 20:23:31 +0100

    Add tests for zotero-bibtize cli utility

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,4 +16,43 @@ def empty_bibtexfile():
         def __init__(self):
             pass
     yield BibTexFileEmpty()
-    
+
+
+@pytest.fixture
+def click_runner():
+    from click.testing import CliRunner
+    yield CliRunner()
+
+
+@pytest.fixture
+def tempfolder():
+    from tempfile import TemporaryDirectory
+    import pathlib
+    tempfolder = TemporaryDirectory()
+    yield pathlib.Path(tempfolder.name)
+
+
+@pytest.fixture
+def tempcwd(tempfolder):
+    import os
+    import pathlib
+    initial_cwd = os.getcwd()
+    # change current working directory to the temporary folder
+    os.chdir(tempfolder)
+    yield tempfolder
+    # change back to the original cwd after test has finished
+    os.chdir(initial_cwd)
+
+
+@pytest.fixture
+def zotero_testfile():
+    import pathlib
+    root = pathlib.Path(__file__).parent
+    yield root / 'cli' / 'test_data' / 'original_entry.bib'
+
+
+@pytest.fixture
+def wanted_testfile():
+    import pathlib
+    root = pathlib.Path(__file__).parent
+    yield root / 'cli' / 'test_data' / 'wanted_entry.bib'
